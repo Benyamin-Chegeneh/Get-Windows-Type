d5e29b17febc537695ddc7f9f9c1189719e5412a
Benyamin Chegeneh <149707803+Benyamin-Chegeneh@users.noreply.github.com>
2024-03-27 01:37:19 +0330

    Add files via upload

---
+++ Get Windows Type.bat
@@ -0,0 +1,10 @@
+@echo off
+TITLE Get Windows Type - Written by Benyamin Chegeneh
+Color a
+for /f "tokens=4-5 delims=. " %%i in ('ver') do set VERSION=%%i.%%j
+if "%version%" == "6.0" echo Windows Vista.
+if "%version%" == "6.1" echo Windows 7
+if "%version%" == "6.2" echo Windows 8
+if "%version%" == "6.3" echo Windows 8.1
+if "%version%" == "10.0" echo Windows 10
+Pause
